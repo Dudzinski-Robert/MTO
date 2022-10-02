ec2700084786283dc63165242563bc33d11d2d07
Robert Dudzinski <dudzinski.robert97@gmail.com>
2022-10-02 12:32:21 +0200

    xxx

--- lab1/main.c
+++ lab1/main.c
@@ -4,7 +4,7 @@
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
+			i++;		
 			printf("%s",param);
 		}else
 			putchar(format_string[i]);
@@ -14,8 +14,10 @@ int my_printf(char *format_string, char *param){
 
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
-	while(gets(buf)){
-		gets(buf2);
+	while(fgets(buf, 1024, stdin)){
+		fgets(buf2, 1024, stdin);
+		buf[strlen(buf)-1] = '\0';
+		buf2[strlen(buf2)-1] = '\0';
 		my_printf(buf,buf2);
 	}
 	return 0;
