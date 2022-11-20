74284a7c819c214298d89fbe2040d0a6840421a2
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2022-11-20 12:01:31 +0100

    Add split string

--- lab5/main.js
+++ lab5/main.js
@@ -3,8 +3,28 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lingeringLine = "";
 
+
 function myPrintf(formatString, param){
-	
+
+    // for(var i=0;i<formatString.length;i++){
+	// 	if((formatString.charAt(i) == '#') && (formatString.charAt(i+1) == 'g')){
+
+    //     }
+	// }
+
+    let splitString = formatString.split('#');
+    if(splitString.length === 1){
+        console.log(formatString);
+        return;
+    }
+
+    const temp = splitString.pop();
+    splitString = [...splitString, ...temp.split('g')];
+
+    if(splitString.length === 2){
+        console.log(formatString);
+        return;
+    }
 }
 
 process.stdin.on('data', function(chunk) {
