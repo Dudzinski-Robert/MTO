53fe0e722024d17b9cf56db28cfe7b51f7141cdc
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2022-11-20 11:18:49 +0100

    Clear code

--- lab5/main.c
+++ lab5/main.c
@@ -3,27 +3,11 @@
 #include <stdlib.h>
 
 
-char *strrev(char *str)
-{
-      char *p1, *p2;
-
-      if (! str || ! *str)
-            return str;
-      for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
-      {
-            *p1 ^= *p2;
-            *p2 ^= *p1;
-            *p1 ^= *p2;
-      }
-      return str;
-}
-
 int my_printf(char *format_string, char *param){
 
 	int myInt = atoi(param);
 	char str[12];
 	sprintf(str, "%d", myInt);
-	strrev(str);
 
 	for(int i=0;i<strlen(format_string);i++){
 		if(format_string[i] == '#'){
@@ -35,8 +19,6 @@ int my_printf(char *format_string, char *param){
 			putchar(format_string[i]);
 		}
 	}
-
-
 	puts("");
 }
 
