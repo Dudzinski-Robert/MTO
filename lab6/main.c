2e12da2ca479fa4e33b7ae66f523928393617f57
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2022-12-11 11:09:20 +0100

    Init lab6

---
+++ lab6/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int my_printf(char *format_string, char *param){
+
+	int myInt = atoi(param);
+	char str[12];
+	sprintf(str, "%d", myInt);
+
+	for(int i=0;i<strlen(format_string);i++){
+		if(format_string[i] == '#'){
+			if(format_string[i + 1] == 'g'){
+				i++;
+				printf("%s", str);
+			}
+		} else {
+			putchar(format_string[i]);
+		}
+	}
+	puts("");
+}
+
+
+int main(int argc, char *argv[]){
+	char buf[1024],buf2[1024];
+	while(fgets(buf, 1024, stdin)){
+		fgets(buf2, 1024, stdin);
+		buf[strlen(buf)-1] = '\0';
+		buf2[strlen(buf2)-1] = '\0';
+		my_printf(buf,buf2);
+	}
+	return 0;
+}
---
+++ lab6/main.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lingeringLine = "";
+
+const shiftParam = (param) => {
+    if(param === undefined){
+        return('');
+    }
+
+    param.split('').map((item) => {
+        if(item === '0'){
+            return '9'
+        }
+
+        return item-1;
+    })
+
+    return param;
+}
+
+
+function myPrintf(formatString, param){
+    let splitString = formatString.split('#');
+    if(splitString.length !== 2){
+        console.log(formatString);
+        return;
+    }
+
+    const temp = splitString.pop();
+    splitString = [...splitString, ...temp.split('g')];
+
+    if(splitString.length !== 3){
+        console.log(formatString);
+        return;
+    }
+
+    let padString = splitString[1][0] === '0' ? '0' : ' ';
+    console.log(splitString[0] + shiftParam(param).padStart(splitString[1], padString) + splitString[2]);
+}
+
+process.stdin.on('data', function(chunk) {
+	lines = chunk.split("\n");
+
+	lines[0] = lingeringLine + lines[0];
+	lingeringLine = lines.pop();
+	for(var i=0;i<lines.length;i++){
+		myPrintf(lines[i],lines[i+1])
+		i++;
+	}
+});
